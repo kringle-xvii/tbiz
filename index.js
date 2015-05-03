379d0b150f110af6eeea58543b1103fef0349cca
Kingle XVII <kingle-xvii@taint.biz>
2015-05-03 13:16:37 -0400

    Adding content from reddit

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 (function() {
 	var musicOptions = [
-		's7L2PVdrb_8',
-		'4MD4BPAp4jo',
-		'REqz9D7OWZE'	
-	],
+			's7L2PVdrb_8',
+			'4MD4BPAp4jo',
+			'REqz9D7OWZE',
+			'sbzvLgvvXa8'
+		],
+		subreddits = [
+			'onetruegod'
+		],
 	youtubeBaseUrl = 'https://www.youtube.com/embed/<%= songId %>?autoplay=1&amp;controls=0' + 
 		'&amp;modestbranding=1&amp;playlist=s7L2PVdrb_8&amp;showinfo=0&amp;start=0&amp;loop=1&amp;enablejsapi=1&amp;';
 		
@@ -16,5 +20,51 @@
 			});
 			$('#player').attr('src', youtubeLink);	
 		}
-	})	
+		
+			var jsonUrl = _.template('http://www.reddit.com/r/<%= subreddit %>.json')({
+				subreddit: _.sample(subreddits)
+			});
+			
+		    $.ajax({
+	            url: jsonUrl,
+	            dataType: 'json',
+	            success: onSuccess,
+				error: function(err) {
+					console.log('TOTAL DESTRUCTION', err);
+				},
+	            timeout: 5000
+	        });
+			
+			function onSuccess(response) {
+				var jqTaintGrid = $('#taint-grid'),
+					getChildElemStr = _.template('<li><img src="<%= url %>"/></li>');
+				
+				var urls = _(response.data.children)
+					.map(function(child) {
+						return child.data.url;	
+					})
+					.filter(function(url) {
+						return _.includes(url, 'imgur') && !_.includes(url, 'gallery');	
+					})
+					.map(function ensureExtension(url) {
+						var extensionRegexp = /\.[a-z]+$/,
+							match = extensionRegexp.exec(url);
+							
+						if (match) {
+							return url;
+						}
+						
+						return url + '.png';
+					})
+					.compact()
+					.valueOf();
+				
+				_.forEach(urls, function(url) {
+					jqTaintGrid.append(
+						getChildElemStr({url: url})
+					);
+				});
+			}
+		}
+	});	
 })();
